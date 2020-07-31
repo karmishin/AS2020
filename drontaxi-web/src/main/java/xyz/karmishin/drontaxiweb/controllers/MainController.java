5bb1233349c7d945632d8ecfc06d414cac83ea6f
Mikhail Karmishin <20360770+karmishin@users.noreply.github.com>
2020-07-31 09:02:15 +0300

    error handling

--- drontaxi-web/src/main/java/xyz/karmishin/drontaxiweb/controllers/MainController.java
+++ drontaxi-web/src/main/java/xyz/karmishin/drontaxiweb/controllers/MainController.java
@@ -1,11 +1,9 @@
 package xyz.karmishin.drontaxiweb.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import xyz.karmishin.drontaxiweb.services.UserService;
 
 @Controller
 public class MainController {
@@ -17,10 +15,5 @@ public class MainController {
     @RequestMapping("/login")
     public String login(Model model) {
         return "login";
-    }
-
-    @GetMapping("/profile")
-    public String profile() {
-        return "profile";
     }
 }
